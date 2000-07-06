f1f96f0dc9d830f197809e910ed58d2988e3ff99
GreyCat <GreyCat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-06 02:59:38 +0000

    Made fingering list somewhat usable, one can add fingerings, there's a list, etc. GREYFIX: fix an array to something more useful.

--- kguitar/fingerlist.h
+++ kguitar/fingerlist.h
@@ -4,21 +4,23 @@
 #include <qtableview.h>
 #include "global.h"
 
+#define ICONCHORD      50
+
 class FingerList: public QTableView
 {
     Q_OBJECT
 public:
     FingerList(QWidget *parent = 0, const char *name = 0);
 
+    void addFingering(const int a[MAX_STRINGS]);
+
 public slots:
 /*     void clear(); */
 /*     void setFirstFret(int fret); */
 
-signals:
-    void chordChange();
-
 protected:
     virtual void paintCell(QPainter *, int row, int col);
+    virtual void resizeEvent(QResizeEvent *); 
 /*     virtual void drawContents(QPainter *); */
 /*     virtual void mouseMoveEvent(QMouseEvent *); */
 /*     virtual void mousePressEvent(QMouseEvent *); */
@@ -26,8 +28,9 @@ protected:
 
 private:
     enum { SCALE=20, CIRCLE=16, CIRCBORD=2, BORDER=5, SPACER=3, FRETTEXT=10 };
-
-    int appl[MAX_STRINGS];
+    
+    int num,perRow;
+    int appl[100][MAX_STRINGS];
 
     int current;
     int numstr;
