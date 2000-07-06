c813a8b5849671f6ae80ae06ed41d143c4bea492
GreyCat <GreyCat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-06 02:59:38 +0000

    Made fingering list somewhat usable, one can add fingerings, there's a list, etc. GREYFIX: fix an array to something more useful.

--- kguitar/kguitar/fingerlist.cpp
+++ kguitar/kguitar/fingerlist.cpp
@@ -1,4 +1,5 @@
 #include "fingerlist.h"
+#include "global.h"
 
 #include <qpainter.h>
 
@@ -6,15 +7,37 @@ FingerList::FingerList(QWidget *parent,const char *name): QTableView(parent,name
 {
   setTableFlags(Tbl_autoVScrollBar | Tbl_smoothScrolling);
   setFrameStyle(Panel | Sunken);
-  setNumRows(3);
-  setNumCols(3);
-  setCellWidth(20);
-  setCellHeight(20);
+  num=0;
+  setCellWidth(ICONCHORD);
+  setCellHeight(ICONCHORD);
+
   repaint();
+}
+
+void FingerList::addFingering(const int a[MAX_STRINGS])
+{
+  for (int i=0;i<MAX_STRINGS;i++)
+    appl[num][i]=a[i];
+  num++;
+  // num is overral number of chord applicatures. If it's 0 - then there are no applicatures.
+  // In the appl array, indexes should be ranged from 0 to (num-1)
+}
+
+void FingerList::resizeEvent(QResizeEvent *e)
+{
+  perRow = width()/ICONCHORD;
+  setNumCols(perRow);
+  setNumRows(num/perRow+1);
 }
 
 void FingerList::paintCell(QPainter *p, int row, int col)
 {
-  p->drawEllipse(0,0,10,10);
-  return;
+  int n = row*perRow+col;
+
+  p->drawRect(0,0,ICONCHORD-1,ICONCHORD-1);
+
+  if (n<num) {
+    p->drawLine(0,0,ICONCHORD-1,ICONCHORD-1);
+    p->drawLine(ICONCHORD-1,0,0,ICONCHORD-1);
+  }
 }
