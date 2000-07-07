f74c67a364f8a041cf6c63b60af407f80683e0a0
GreyCat <GreyCat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-07 00:47:13 +0000

    Added some sort of real, not dummy fingerlist displayer.

--- kguitar/fingerlist.cpp
+++ kguitar/fingerlist.cpp
@@ -8,6 +8,9 @@ FingerList::FingerList(QWidget *parent,const char *name): QTableView(parent,name
   setTableFlags(Tbl_autoVScrollBar | Tbl_smoothScrolling);
   setFrameStyle(Panel | Sunken);
   num=0;
+
+  numstr=6; // GREYFIX
+
   setCellWidth(ICONCHORD);
   setCellHeight(ICONCHORD);
 
@@ -45,7 +48,82 @@ void FingerList::paintCell(QPainter *p, int row, int col)
   p->drawRect(0,0,ICONCHORD-1,ICONCHORD-1);
 
   if (n<num) {
-    p->drawLine(0,0,ICONCHORD-1,ICONCHORD-1);
-    p->drawLine(ICONCHORD-1,0,0,ICONCHORD-1);
+    int barre,eff;
+    
+    // Horizontal lines
+    
+    for (int i=0;i<=NUMFRETS;i++)
+      p->drawLine(SCALE/2+BORDER+FRETTEXT,BORDER+SCALE+2*SPACER+i*SCALE,
+		  SCALE/2+BORDER+numstr*SCALE-SCALE+FRETTEXT,BORDER+SCALE+2*SPACER+i*SCALE);
+    
+    // Beginning fret number
+    
+    int firstFret=24;
+    bool noff=TRUE;
+
+    for (int i=0;i<numstr;i++) {
+      if ((appl[n][i]<firstFret) && (appl[n][i]>0))
+	firstFret=appl[n][i];
+      if (appl[n][i]>5)
+	noff=FALSE;
+    }
+
+    if (noff)
+      firstFret=1;
+
+    if (firstFret>1) {
+      QString fs;
+      fs.setNum(firstFret);
+      p->drawText(BORDER,BORDER+SCALE+2*SPACER,50,50,AlignLeft | AlignTop,fs);
+    }
+
+    // Vertical lines and fingering
+    
+    for (int i=0;i<numstr;i++) {
+      p->drawLine(i*SCALE+BORDER+SCALE/2+FRETTEXT,BORDER+SCALE+2*SPACER,
+		  i*SCALE+BORDER+SCALE/2+FRETTEXT,BORDER+SCALE+2*SPACER+NUMFRETS*SCALE);
+      if (appl[n][i]==-1) {
+	p->drawLine(i*SCALE+BORDER+CIRCBORD+FRETTEXT,BORDER+CIRCBORD,
+		    i*SCALE+BORDER+SCALE-CIRCBORD+FRETTEXT,BORDER+SCALE-CIRCBORD);
+	p->drawLine(i*SCALE+BORDER+SCALE-CIRCBORD+FRETTEXT,BORDER+CIRCBORD,
+		    i*SCALE+BORDER+CIRCBORD+FRETTEXT,BORDER+SCALE-CIRCBORD);
+      } else if (appl[n][i]==0) {
+	p->setBrush(NoBrush);
+	p->drawEllipse(i*SCALE+BORDER+CIRCBORD+FRETTEXT,BORDER+CIRCBORD,CIRCLE,CIRCLE);
+      } else {
+	p->setBrush(SolidPattern);
+	p->drawEllipse(i*SCALE+BORDER+CIRCBORD+FRETTEXT,BORDER+SCALE+2*SPACER+(appl[n][i]-firstFret)*SCALE+
+		       CIRCBORD,CIRCLE,CIRCLE);
+      };
+    }
+    
+    // Analyze & draw barre
+    
+    p->setBrush(SolidPattern);
+    
+    for (int i=0;i<NUMFRETS;i++) {
+      barre=0;
+      while ((appl[n][numstr-barre-1]>=(i+firstFret)) || (appl[n][numstr-barre-1]==-1)) {
+	barre++;
+	if (barre>numstr-1)
+	  break;
+      }
+      
+      while ((appl[n][numstr-barre]!=(i+firstFret)) && (barre>1))
+	barre--;
+      
+      eff=0;
+      for (int j=numstr-barre;j<numstr;j++) {
+	if (appl[n][j]!=-1)
+	  eff++;
+      }
+      
+      if (eff>2) {
+	p->drawRect((numstr-barre)*SCALE+SCALE/2+BORDER+FRETTEXT,BORDER+SCALE+2*SPACER+i*SCALE+CIRCBORD,
+		    (barre-1)*SCALE,CIRCLE);
+      }
+    }  
+
+    p->setBrush(NoBrush);
   }
 }
