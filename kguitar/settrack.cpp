d647c897d1a57c0604347e228dd3b8222a55568d
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-27 00:48:08 +0000

    Track setup dialog basic version

---
+++ kguitar/settrack.cpp
@@ -0,0 +1,62 @@
+#include "settrack.h"
+#include "settabfret.h"
+
+#include <kapp.h>
+#include <qlayout.h>
+#include <qlineedit.h>
+#include <kintegerline.h>
+#include <qlabel.h>
+#include <qpushbutton.h>
+
+SetTrack::SetTrack(QWidget *parent=0, const char *name=0): QDialog(parent,name,TRUE)
+{
+    QVBoxLayout *l = new QVBoxLayout(this,10);
+
+    // GENERAL CONTROLS
+
+    QGridLayout *g = new QGridLayout(3,2,10);
+    l->addLayout(g);
+
+    title = new QLineEdit(this);
+    bank = new KIntegerLine(this);
+    patch = new KIntegerLine(this);
+    QLabel *title_l = new QLabel(title,i18n("&Track name:"),this);
+    QLabel *bank_l = new QLabel(bank,i18n("&Bank:"),this);
+    QLabel *patch_l = new QLabel(patch,i18n("&Patch:"),this);
+
+    g->addWidget(title_l,0,0);
+    g->addWidget(title,0,1);
+    g->addWidget(bank_l,1,0);
+    g->addWidget(bank,1,1);
+    g->addWidget(patch_l,2,0);
+    g->addWidget(patch,2,1);
+
+    for (int i=0;i<3;i++) 
+	g->addRowSpacing(i,20);
+
+    g->addColSpacing(0,80);
+    g->setColStretch(1,1);
+
+    // TAB MODE SPECIFIC WIDGET
+
+    fret = new SetTabFret(this);
+    l->addWidget(fret,1);
+
+    // DIALOG BUTTONS
+
+    QHBoxLayout *butt = new QHBoxLayout();
+    l->addLayout(butt);
+
+    QPushButton *ok = new QPushButton(i18n("OK"),this);
+    connect(ok,SIGNAL(clicked()),SLOT(accept()));
+    QPushButton *cancel = new QPushButton(i18n("Cancel"),this);
+    connect(cancel,SIGNAL(clicked()),SLOT(reject()));
+
+    butt->addWidget(ok);
+    butt->addWidget(cancel);
+    butt->addStrut(30);
+    
+    l->activate();
+
+    setCaption(i18n("Track properties"));
+}
