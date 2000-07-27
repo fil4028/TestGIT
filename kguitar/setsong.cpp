e3578f21dfc2a77ec9998820ff4b0448d0b13d83
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-27 00:50:18 +0000

    Added layout manager things. Much better

--- kguitar/setsong.cpp
+++ kguitar/setsong.cpp
@@ -5,36 +5,50 @@
 #include <qmultilinedit.h>
 #include <qlabel.h>
 #include <qpushbutton.h>
+#include <qlayout.h>
 
 SetSong::SetSong(QWidget *parent=0, const char *name=0): QDialog(parent,name,TRUE)
 {
     title = new QLineEdit(this);
-    title->setGeometry(100,10,290,20);
-    QLabel *title_l = new QLabel(title,i18n("&Title:"),this);
-    title_l->setGeometry(10,10,90,20);
-
     author = new QLineEdit(this);
-    author->setGeometry(100,40,290,20);
-    QLabel *author_l = new QLabel(author,i18n("&Author:"),this);
-    author_l->setGeometry(10,40,90,20);
-
     transcriber = new QLineEdit(this);
-    transcriber->setGeometry(100,70,290,20);
-    QLabel *transcriber_l = new QLabel(transcriber,i18n("&Transcriber:"),this);
-    transcriber_l->setGeometry(10,70,90,20);
-
     comments = new QMultiLineEdit(this);
-    comments->setGeometry(100,100,290,150);
+
+    QLabel *title_l = new QLabel(title,i18n("&Title:"),this);
+    QLabel *author_l = new QLabel(author,i18n("&Author:"),this);
+    QLabel *transcriber_l = new QLabel(transcriber,i18n("&Transcriber:"),this);
     QLabel *comments_l = new QLabel(comments,i18n("&Comments:"),this);
-    comments_l->setGeometry(10,100,90,20);
 
     QPushButton *ok = new QPushButton(i18n("OK"),this);
-    ok->setGeometry(10,260,80,30);
     connect(ok,SIGNAL(clicked()),SLOT(accept()));
     QPushButton *cancel = new QPushButton(i18n("Cancel"),this);
-    cancel->setGeometry(100,260,80,30);
     connect(cancel,SIGNAL(clicked()),SLOT(reject()));
 
+    QGridLayout *g = new QGridLayout(this,5,2,10);
+    g->addWidget(title_l,0,0);
+    g->addWidget(title,0,1);
+    g->addWidget(author_l,1,0);
+    g->addWidget(author,1,1);
+    g->addWidget(transcriber_l,2,0);
+    g->addWidget(transcriber,2,1);
+    g->addWidget(comments_l,3,0);
+    g->addWidget(comments,3,1);
+
+    QHBoxLayout *butt = new QHBoxLayout();
+    g->addLayout(butt,4,1);
+    butt->addWidget(ok);
+    butt->addWidget(cancel);
+
+    g->setRowStretch(3,1);
+    g->setColStretch(1,1);
+    g->addColSpacing(0,90);
+    for (int i=0;i<4;i++)
+	g->addRowSpacing(i,20);
+    g->addRowSpacing(4,30);
+
+    g->activate();
+
+    setMinimumSize(250,170);
+    resize(400,300);
     setCaption(i18n("Song properties"));
-    setFixedSize(400,300);
 }
