bafe2ace8ebb6864202fd3c20831b047d22a4db7
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-27 00:48:37 +0000

    Guitar tab track setup dialog basic version

---
+++ kguitar/kguitar/settabfret.h
@@ -0,0 +1,29 @@
+#ifndef SETTABFRET_H
+#define SETTABFRET_H
+
+#include <qgroupbox.h>
+#include "global.h"
+
+class QSpinBox;
+class QComboBox;
+class RadiusTuner;
+class QVBoxLayout;
+
+class SetTabFret: public QGroupBox
+{
+    Q_OBJECT
+public:
+    SetTabFret(QWidget *parent=0, const char *name=0);
+
+public slots:
+    void setLibTuning(int n);
+    void stringChanged(int n);
+
+private:
+    QComboBox *lib;
+    QSpinBox *st;
+    QVBoxLayout *l;
+    RadiusTuner *tuner[MAX_STRINGS];
+};
+
+#endif
