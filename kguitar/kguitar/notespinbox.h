0755742b47c37953b9f71c83f427e12b467ab239
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-30 00:18:29 +0000

    Basic widget & validators for spinbox with MIDI note specifier.

---
+++ kguitar/kguitar/notespinbox.h
@@ -0,0 +1,31 @@
+#ifndef NOTESPINBOX_H
+#define NOTESPINBOX_H
+
+#include <qwidget.h>
+#include "global.h"
+
+#include <qvalidator.h>
+#include <qspinbox.h>
+
+class NoteValidator: public QValidator
+{
+    Q_OBJECT
+public:
+    NoteValidator(QWidget *parent, const char *name=0);
+    virtual State validate(QString &input, int &pos);
+};
+
+class NoteSpinBox: public QSpinBox
+{
+    Q_OBJECT
+public:
+    NoteSpinBox(QWidget *parent=0, const char *name=0);
+    ~NoteSpinBox();
+private:
+    NoteValidator *nv;
+
+    virtual QString mapValueToText(int v);
+    virtual int mapTextToValue(bool *ok);
+};
+
+#endif
