ae89afd9f8082360a9fdd958c8850e7a029dfda5
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-07-30 00:18:29 +0000

    Basic widget & validators for spinbox with MIDI note specifier.

---
+++ kguitar/notespinbox.cpp
@@ -0,0 +1,89 @@
+#include "notespinbox.h"
+
+#include <qvalidator.h>
+
+NoteValidator::NoteValidator(QWidget *parent, const char *name=0):
+    QValidator(parent,name)
+{
+}
+
+QValidator::State NoteValidator::validate(QString &input, int &pos)
+{
+    State res = Invalid;
+
+    switch (input.length()) {
+    case 1:
+	if ((input.left(1)>='A') && (input.left(1)<='H'))
+	    res = Valid;
+	break;
+    case 2:
+	if ((input.left(1)>='A') && (input.left(1)<='H')) {
+	    if (input.mid(1,1)=='#') {
+		res = Valid;
+	    } else if ((input.mid(1,1)>='0') && (input.mid(1,1)<='9')) {
+		res = Acceptable;
+	    } else {
+		res = Invalid;
+	    }
+	}
+	break;
+    case 3:
+	if ((input.left(1)>='A') && (input.left(1)<='H') &&
+	    (input.mid(1,1)=='#') &&
+	    (input.mid(2,1)>='0') && (input.mid(2,1)<='9')) {
+	    res = Acceptable;
+	} else {
+	    res = Invalid;
+	}
+    }
+
+    return res;
+}
+
+NoteSpinBox::NoteSpinBox(QWidget *parent=0, const char *name=0):
+    QSpinBox(0,255,1,parent,name)
+{
+    nv = new NoteValidator(this);
+    setValidator(nv);
+}
+
+NoteSpinBox::~NoteSpinBox()
+{
+    delete nv;
+}
+
+QString NoteSpinBox::mapValueToText(int v)
+{
+    QString tmp;
+
+    tmp.setNum(v/12);
+    tmp=note_name(v%12)+tmp;
+
+    return tmp;
+}
+
+int NoteSpinBox::mapTextToValue(bool *ok)
+{
+    if (!ok)
+	return 0;
+
+    QString t = text();
+    QString nn;
+
+    if (t.data()[1]=='#') {
+	nn=t.left(2);
+    } else {
+	nn=t.left(1);
+    }
+
+    int cn=-1;
+    
+    for (int i=0;i<12;i++)
+	if (nn==note_name(i))
+	    cn=i;
+
+    nn=t.right(1);
+    int oct = nn.toInt();
+
+    return oct*12+cn;
+}
