e1ede4462ae3b7934178894c0296f4f8743adc8c
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-08-03 23:55:58 +0000

    Added global options

--- kguitar/global.h
+++ kguitar/global.h
@@ -1,5 +1,14 @@
+// Global defines
+
 #define MAX_STRINGS   12
 #define MAX_FRETS     24
 #define NUMFRETS      5
 
+// Global utility functions
+
 QString note_name(int);
+
+// Global options
+
+extern int global_maj7;
+extern int global_flatplus;
