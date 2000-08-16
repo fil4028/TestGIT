6f6f4fd82422a3dd1967e716498bf29918fd59b5
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-08-16 00:34:23 +0000

    Transferred all global options from global.h to globaloptions.h - should help with easier rebuilding...

---
+++ kguitar/globaloptions.h
@@ -0,0 +1,11 @@
+// Global options
+
+extern int global_maj7;
+extern int global_flatplus;
+extern int global_notenames;
+
+// Features for global options
+
+extern QString maj7name[];
+extern QString flat[];
+extern QString sharp[];
