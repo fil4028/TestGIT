1dabd86fc390910ef5cef74e10fd40ff153c3b79
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-08-17 01:04:50 +0000

    Renamed track.h to tabsong.h

---
+++ kguitar/kguitar/tabsong.h
@@ -0,0 +1,37 @@
+#ifndef TABSONG_H
+#define TABSONG_H
+
+#include <qlist.h>
+#include <qstring.h>
+#include <qtextstream.h>
+
+#include "global.h"
+
+#include "tabtrack.h"
+
+class TabSong
+{
+public:
+    TabSong(QString _title, int _tempo) { tempo=_tempo;title=_title;t.setAutoDelete(TRUE); };
+    int tempo;
+    QList<TabTrack> t;                  // Track data
+    QString title;                      // Title of the song
+    QString author;                     // Author of the tune
+    QString transcriber;                // Who made the tab
+    QString comments;                   // Comments
+
+    QString filename;                   // File name to save under
+
+    bool load_from_kg(QString fileName);        // Native format - kg
+    bool save_to_kg(QString fileName);
+    bool load_from_gtp(QString fileName);       // Guitar Pro format
+    bool save_to_gtp(QString fileName);
+    bool load_from_mid(QString fileName);       // MIDI files
+    bool save_to_mid(QString fileName);
+    bool load_from_tab(QString fileName);       // ASCII tabulatures
+    bool save_to_tab(QString fileName);
+private:
+    void writeCentered(QTextStream *s, QString l);
+};
+
+#endif
