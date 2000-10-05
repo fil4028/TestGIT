0d9568753d1f108d79bdeed4d63aaf8c6d0792bd
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-10-05 05:43:32 +0000

    Make a better strumming scheme.

--- kguitar/kguitar/strumlib.h
+++ kguitar/kguitar/strumlib.h
@@ -1,13 +1,24 @@
 // Strumming library defines and headers
 
-#define MAX_STRUM_LENGTH	8
+#define MAX_STRUM_LENGTH	10
 
-#define ST1			1
-#define ST2			2
-#define ST3			4
-#define ST4			8
-#define ST5			16
-#define ST6			32
+// Treble order
+
+#define TR1			1
+#define TR2			2
+#define TR3			4
+#define TR4			8
+#define TR5			16
+#define TR6			32
+
+// Bass order
+
+#define BS1			-1
+#define BS2			-2
+#define BS3			-4
+#define BS4			-8
+#define BS5			-16
+#define BS6			-32
 
 typedef struct {
 	int mask[MAX_STRUM_LENGTH];
