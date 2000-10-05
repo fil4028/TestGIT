b9846b7b1bd67e8a7ef809fbd04581bc4828d1f8
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-10-05 05:43:32 +0000

    Make a better strumming scheme.

--- kguitar/strum.h
+++ kguitar/strum.h
@@ -5,8 +5,14 @@
 // The library itself.
 
 // First field of record should be an array with bitmasked strum
-// pattern. Please add up ST macros with "|" operator to get the
-// proper results. See the examples below.
+// pattern. Please add up TR and BS macros with "+" operator to get
+// the proper results. See the examples below.
+
+// TR macros reference strings from highest to lowest, i.e. TR1
+// references the top tab line with standard 6-string guitar.
+
+// BS macros reference strings vice versa, from lowest to highest,
+// i.e. BS1 references the bottom tab line with guitar.
 
 // Second field should be with durations of notes of corresponding
 // bitmasked patterns in the first array, all in MIDI note length
@@ -19,19 +25,25 @@ strummer lib_strum[] = {
 	{ {},
 	  { 1 },
 	  "Chord" },
-	{ { ST1, ST4, ST5, ST6, ST5, ST4 },
+	{ { BS1, TR3, TR2, TR1, TR2, TR3 },
 	  { 60,  60,  60,  60,  60,  60, 0 },
 	  "6/8 Arpegio" },
-	{ { ST1, ST4, ST5 | ST6, ST4 },
+	{ { BS1, TR3, TR2 + TR1, TR3 },
 	  { 120, 120, 120,       120, 0 },
 	  "4/4 Arpegio" },
-	{ { ST1, ST4 | ST5 | ST6, ST2, ST4 | ST5 | ST6 },
+	{ { BS1, TR3, TR2, TR3, TR1, TR3, TR2, TR3 },
+	  { 60,  60,  60,  60,  60,  60,  60,  60, 0 },
+	  "8/8 Arpegio 1" },
+	{ { BS1, TR3, TR2, TR3, TR1, TR2, TR3, TR2 },
+	  { 60,  60,  60,  60,  60,  60,  60,  60, 0 },
+	  "8/8 Arpegio 2" },
+	{ { BS1, TR1 + TR2 + TR3, BS2, TR1 + TR2 + TR3 },
 	  { 120, 120,             120, 120, 0 },
 	  "4/4 Normal Bass Pick" },
-	{ { ST1, ST4 | ST5 | ST6, ST4 | ST5 | ST6 },
+	{ { BS1, TR1 + TR2 + TR3, TR1 + TR2 + TR3 },
 	  { 120, 120,             120, 0 },
 	  "3/4 Waltz Bass Pick" },
-	{ { ST1, ST4 | ST5 | ST6, ST2, ST4 | ST5 | ST6 },
+	{ { BS1, TR1 + TR2 + TR3, BS2, TR1 + TR2 + TR3 },
 	  { 180, 60,              180, 60, 0 },
 	  "4/4 Deep Bass Pick" },
 
