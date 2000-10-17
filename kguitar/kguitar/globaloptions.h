b2f1158db8a0cadd36a94dd7a1d2b8ae8b7a6dfa
alinx <alinx@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-10-17 22:11:28 +0000

    added store window & toolbar geometry toolbar status in configs

--- kguitar/kguitar/globaloptions.h
+++ kguitar/kguitar/globaloptions.h
@@ -1,13 +1,25 @@
 // Global options
 
+// General
 extern int global_maj7;
 extern int global_flatplus;
 extern int global_notenames;
 
-extern int global_tabsize;         // for MusiXTeX export
+// MusiXTeX
+extern int global_tabsize;
 extern bool global_showbarnumb;
 extern bool global_showstr;
 extern bool global_showpagenumb;
+
+// Appearance
+extern bool global_showMainTB;
+extern bool global_showEditTB;
+extern int global_mainTBPos;
+extern int global_editTBPos;
+extern int global_mainWinWidth;
+extern int global_mainWinHeight;
+extern int global_mainWinX;
+extern int global_mainWinY;
 
 // Features for global options
 
