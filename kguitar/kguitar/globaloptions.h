cca07ef38222d879a45d0111734a2c26371fef54
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-10-28 20:48:43 +0000

    Fixed global options case

--- kguitar/kguitar/globaloptions.h
+++ kguitar/kguitar/globaloptions.h
@@ -1,21 +1,25 @@
 // Global options
 
 // General
-extern int global_maj7;
-extern int global_flatplus;
-extern int global_notenames;
+extern int globalMaj7;
+extern int globalFlatPlus;
+extern int globalNoteNames;
 
 // MusiXTeX
-extern int global_tabsize;
-extern bool global_showbarnumb;
-extern bool global_showstr;
-extern bool global_showpagenumb;
+extern int globalTabSize;
+extern bool globalShowBarNumb;
+extern bool globalShowStr;
+extern bool globalShowPageNumb;
 
 // Appearance
-extern bool global_showMainTB;
-extern bool global_showEditTB;
-extern int global_mainTBPos;
-extern int global_editTBPos;
+extern bool globalShowMainTB;
+extern bool globalShowEditTB;
+extern int globalMainTBPos;
+extern int globalEditTBPos;
+
+// ALSA MIDI
+extern int globalAlsaClient;
+extern int globalAlsaPort;
 
 // Features for global options
 
