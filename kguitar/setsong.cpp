643618d8f0aa541dd17a3285e5b62530d47c9297
alinx <alinx@1d129ebe-d642-0410-a05f-df7310e4b27a>
2000-12-20 12:51:16 +0000

    Fixed code style

--- kguitar/setsong.cpp
+++ kguitar/setsong.cpp
@@ -8,48 +8,48 @@
 #include <qpushbutton.h>
 #include <qlayout.h>
 
-SetSong::SetSong(QWidget *parent=0, const char *name=0): QDialog(parent,name,TRUE)
+SetSong::SetSong(QWidget *parent = 0, const char *name = 0): QDialog(parent, name, TRUE)
 {
-    title = new QLineEdit(this);
-    author = new QLineEdit(this);
-    transcriber = new QLineEdit(this);
-    comments = new QMultiLineEdit(this);
+	title = new QLineEdit(this);
+	author = new QLineEdit(this);
+	transcriber = new QLineEdit(this);
+	comments = new QMultiLineEdit(this);
 
-    QLabel *title_l = new QLabel(title,i18n("&Title:"),this);
-    QLabel *author_l = new QLabel(author,i18n("&Author:"),this);
-    QLabel *transcriber_l = new QLabel(transcriber,i18n("&Transcriber:"),this);
-    QLabel *comments_l = new QLabel(comments,i18n("&Comments:"),this);
+	QLabel *title_l = new QLabel(title, i18n("&Title:"), this);
+	QLabel *author_l = new QLabel(author, i18n("&Author:"), this);
+	QLabel *transcriber_l = new QLabel(transcriber, i18n("&Transcriber:"), this);
+	QLabel *comments_l = new QLabel(comments, i18n("&Comments:"), this);
 
-    QPushButton *ok = new QPushButton(i18n("OK"),this);
-    connect(ok,SIGNAL(clicked()),SLOT(accept()));
-    QPushButton *cancel = new QPushButton(i18n("Cancel"),this);
-    connect(cancel,SIGNAL(clicked()),SLOT(reject()));
+	QPushButton *ok = new QPushButton(i18n("OK"), this);
+	connect(ok, SIGNAL(clicked()), SLOT(accept()));
+	QPushButton *cancel = new QPushButton(i18n("Cancel"), this);
+	connect(cancel, SIGNAL(clicked()), SLOT(reject()));
 
-    QGridLayout *g = new QGridLayout(this,5,2,10);
-    g->addWidget(title_l,0,0);
-    g->addWidget(title,0,1);
-    g->addWidget(author_l,1,0);
-    g->addWidget(author,1,1);
-    g->addWidget(transcriber_l,2,0);
-    g->addWidget(transcriber,2,1);
-    g->addWidget(comments_l,3,0);
-    g->addWidget(comments,3,1);
+	QGridLayout *g = new QGridLayout(this, 5, 2, 10);
+	g->addWidget(title_l, 0, 0);
+	g->addWidget(title, 0, 1);
+	g->addWidget(author_l, 1, 0);
+	g->addWidget(author, 1, 1);
+	g->addWidget(transcriber_l, 2, 0);
+	g->addWidget(transcriber, 2, 1);
+	g->addWidget(comments_l, 3, 0);
+	g->addWidget(comments, 3, 1);
 
-    QHBoxLayout *butt = new QHBoxLayout();
-    g->addLayout(butt,4,1);
-    butt->addWidget(ok);
-    butt->addWidget(cancel);
+	QHBoxLayout *butt = new QHBoxLayout();
+	g->addLayout(butt, 4, 1);
+	butt->addWidget(ok);
+	butt->addWidget(cancel);
 
-    g->setRowStretch(3,1);
-    g->setColStretch(1,1);
-    g->addColSpacing(0,90);
-    for (int i=0;i<4;i++)
-	g->addRowSpacing(i,20);
-    g->addRowSpacing(4,30);
+	g->setRowStretch(3, 1);
+	g->setColStretch(1, 1);
+	g->addColSpacing(0, 90);
+	for (int i = 0; i < 4; i++)
+		g->addRowSpacing(i,20);
+	g->addRowSpacing(4,30);
 
-    g->activate();
+	g->activate();
 
-    setMinimumSize(250,170);
-    resize(400,300);
-    setCaption(i18n("Song properties"));
+	setMinimumSize(250, 170);
+	resize(400, 300);
+	setCaption(i18n("Song properties"));
 }
