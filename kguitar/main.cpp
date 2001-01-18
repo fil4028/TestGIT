d280af1b2588b8e1595c6cb50298d240c728aacc
alinx <alinx@1d129ebe-d642-0410-a05f-df7310e4b27a>
2001-01-18 14:23:01 +0000

    Minor tweaks

--- kguitar/main.cpp
+++ kguitar/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 {
 	KAboutData aboutData("kguitar", I18N_NOOP("KGuitar"),
 						 VERSION, DESCRIPTION, KAboutData::License_GPL,
-						 "(C) 2000 by KGuitar Development Team", 0,
+						 "(C) 2000, 2001 by KGuitar Development Team", 0,
 						 "http://kguitar.sourceforge.net");
 
 	aboutData.addAuthor("Mikhail Yakshin AKA GreyCat", I18N_NOOP("Maintainer and main coder"),
