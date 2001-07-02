61e15aac3a22b9a6db3ca61b2327052f8dd81f31
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2001-07-02 07:03:59 +0000

    Fixed formatting & added some new strumming schemes

--- kguitar/strum.h
+++ kguitar/strum.h
@@ -27,6 +27,8 @@
 // pros and cons, complexity, what songs it sounds best in and
 // possibly a well-known song where it is used or originated from.
 
+// Both 3rd and 4th fields should be in i18n()'s.
+
 strummer lib_strum[] = {
 	{ {},
 	  { 1 },
@@ -36,70 +38,84 @@ strummer lib_strum[] = {
 	  { 60,  60,  60,  60,  60,  60, 0 },
 	  i18n("6/8 Standard Arpegio"),
 	  i18n("Classic arpegio, played in 6/8. Makes very good accompaniment "
-	  "for slow, lyric songs. Used in the raw form (playing open "
-	  "strings) by Metallica in the song \"Nothing Else Matters\"") },
+	       "for slow, lyric songs. Used in the raw form (playing open "
+	       "strings) by Metallica in the song \"Nothing Else Matters\"") },
 	{ { BS1, TR3, TR2 + TR1, TR3 },
 	  { 120, 120, 120,       120, 0 },
 	  i18n("4/4 Short Arpegio"),
 	  i18n("Shortened variation of classic arpegio, with chord-like two "
-	  "simultaneous string pick in the middle. A cross between classic "
-	  "arpegio and bass pick technique.") },
+	       "simultaneous string pick in the middle. A cross between classic "
+	       "arpegio and bass pick technique.") },
 	{ { BS1, TR3, TR2, TR3, TR1, TR3, TR2, TR3 },
 	  { 60,  60,  60,  60,  60,  60,  60,  60, 0 },
 	  i18n("8/8 Arpegio 1"),
 	  i18n("Pretty complex arpegio that makes dynamic progressing and "
-	  "continuous rhythm line. Fits best in medium tempo upbeat songs.") },
+	       "continuous rhythm line. Fits best in medium tempo upbeat songs.") },
 	{ { BS1, TR3, TR2, TR3, TR1, TR2, TR3, TR2 },
 	  { 60,  60,  60,  60,  60,  60,  60,  60, 0 },
 	  i18n("8/8 Arpegio 2"),
 	  i18n("Variantion of previous arpegio, can spice up sound if used "
-	  "several times with previous rhythm.") },
+	       "several times with previous rhythm.") },
 	{ { BS1, TR4, TR3, TR2, TR1, TR2, TR3 },
 	  { 60,  30,  30,  60,  60,  60,  60, 0 },
 	  i18n("3/4 Smooth Arpegio With Return"),
 	  i18n("Smooth accelerating and decelarating arpegio. Played mostly "
-	  "with a pick, strumming relatively slow through the strings. "
-	  "Well-known from a song \"House of the Rising Sun\", performed "
-	  "by Animals and many others.") },
+	       "with a pick, strumming relatively slow through the strings. "
+	       "Well-known from a song \"House of the Rising Sun\", performed "
+	       "by Animals and many others.") },
 	{ { BS1, BS2, BS3, TR1, TR2, TR3 },
 	  { 120, 60,  60,  60,  60,  120, 0 },
 	  i18n("4/4 Rock Ballad Arpegio 1"),
-	  i18n("Most famous rock arpegio. If played in a correct way, could make "
-	  "a smooth continuous rhythm, good for verses. One of the most "
-	  "well-known songs that uses such arpegio is \"Don\'t Cry\" by "
-	  "Guns\'n\'Roses.")	},
+	  i18n("One of the most famous rock arpegios. If played in a correct "
+	       "way, could make a smooth continuous rhythm, good for verses. One "
+	       "of the most well-known songs that uses such arpegio is \"Don\'t Cry\" by "
+	       "Guns\'n\'Roses.") },
 	{ { BS1, BS2, BS3, TR1, TR4, TR2, TR4 },
 	  { 60,  60,  60,  120, 60,  60,  60, 0 },
 	  i18n("4/4 Rock Ballad Arpegio 2"),
 	  i18n("Smooth rock ballad arpegio. Picking pattern could seem a bit "
-	  "random-like, though the whole arpegio makes a pretty smooth "
-	  "rhythm. Good for slow and lyric ballads. Performed by Smokie "
-	  "in their song \"Living Next Door to Alice\"") },
+	       "random-like, though the whole arpegio makes a pretty smooth "
+	       "rhythm. Good for slow and lyric ballads. Performed by Smokie "
+	       "in their song \"Living Next Door to Alice\"") },
 	{ { BS1, TR1, TR2, TR3, TR1, TR2, TR3, TR1 },
 	  { 60,  60,  60,  60,  60,  60,  60,  60, 0 },
 	  i18n("4/4 Rock Ballad Arpegio 3"),
 	  i18n("Weird, but simple combination of classic arpegios, makes a "
-	  "good continuous rock lyric arpegio, with a bit of spanish "
-	  "feel. Used in the song of Scorpions \"Holiday\"") },
+	       "good continuous rock lyric arpegio, with a bit of spanish "
+	       "feel. Used in the song of Scorpions \"Holiday\"") },
+	{ { TR1 + TR4, TR2, TR3, TR2 },
+	  { 120,       120, 120, 120, 0 },
+	  i18n("4/4 Rock Ballad Arpegio 4"),
+	  i18n("Classic stairway rock arpegio, this thing was the learning pattern "
+	       "for lots of various musicians. Very regular 4/4, first accented "
+	       "note is a good metronome. Used in (and most likely originating "
+	       "from) Led Zeppelin\'s \"Stairway to Heaven\"") },
+	{ { BS1 + BS4, BS2, BS3, BS1, BS4, BS2, BS3 },
+	  { 120,       60,  60,  60,  60,  60,  60, 0 },
+	  i18n("4/4 Rock Ballad Arpegio 5"),
+	  i18n("Folk style picking, named \"Travis picking\" after Merle Travis "
+	       "who apparently came up with it first. Arpegio line is somewhat "
+	       "syncopated, giving a special feel. Good example of such picking "
+	       "is Kansas' \"Dust in the Wind\"") },
 	{ { BS1, TR1 + TR2 + TR3, BS2, TR1 + TR2 + TR3 },
 	  { 120, 120,             120, 120, 0 },
 	  i18n("4/4 Normal Bass Pick"),
 	  i18n("Classic bass pick technique with alteration. Played in 2 steps: "
-	  "at the first step some sort of bass note is picked and at the "
-	  "second step the bunch of trebles sounds. Very popular thing for "
-	  "lyric songs on acoustic (especially 7-string) guitar.") },
+	       "at the first step some sort of bass note is picked and at the "
+	       "second step the bunch of trebles sounds. Very popular thing for "
+	       "lyric songs on acoustic (especially 7-string) guitar.") },
 	{ { BS1, TR1 + TR2 + TR3, TR1 + TR2 + TR3 },
 	  { 120, 120,             120, 0 },
 	  i18n("3/4 Waltz Bass Pick"),
 	  i18n("Waltz style bass pick. Makes a perfect dance rhythm - played on "
-	  "3/4, with the first beat accented by bass note. Could be used in "
-	  "classic guitar playing experience, for example, in orchestra.") },
+	       "3/4, with the first beat accented by bass note. Could be used in "
+	       "classic guitar playing experience, for example, in orchestra.") },
 	{ { BS1, TR1 + TR2 + TR3, BS2, TR1 + TR2 + TR3 },
 	  { 180, 60,              180, 60, 0 },
 	  i18n("4/4 Deep Bass Pick"),
 	  i18n("Normal bass pick with a shuffled, bluesey feel, made by delaying "
-	  "bass notes by an extra 1/8. A good simple rhythm for blues and "
-	  "folk songs.") },
-
+	       "bass notes by an extra 1/8. A good simple rhythm for blues and "
+	       "folk songs.") },
+	
 	{ {}, { 0 }, "", "" }
 };
