4e0fa310e864792ee66791470c7f025419309465
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2001-07-08 12:45:09 +0000

    Added for drum track mode

---
+++ kguitar/kguitar/settabdrum.h
@@ -0,0 +1,33 @@
+#ifndef SETTABDRUM_H
+#define SETTABDRUM_H
+
+#include <qwidget.h>
+#include "global.h"
+
+#include <qspinbox.h>
+
+class QComboBox;
+
+class SetTabDrum: public QWidget {
+    Q_OBJECT
+public:
+    SetTabDrum(QWidget *parent=0, const char *name=0);
+
+    void setDrums(int n) { dr->setValue(n); };
+    void setTune(uchar x, uchar n) { tuner[x]->setValue(n); };
+    int drums() { return dr->value(); };
+    uchar tune(uchar x) { return tuner[x]->value(); };
+
+public slots:
+    void stringChanged(int n);
+
+private:
+    virtual void resizeEvent(QResizeEvent *e);
+    void reposTuners();
+
+    QSpinBox *dr;
+    QSpinBox *tuner[MAX_STRINGS];
+    int oldst;
+};
+
+#endif
