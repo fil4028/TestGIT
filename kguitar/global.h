9ebbd470953f5aa9c3c085e5522ea1e7569773ba
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2001-11-08 11:51:04 +0000

    Added drum abbreviations

--- kguitar/global.h
+++ kguitar/global.h
@@ -14,5 +14,7 @@ class QString;
 QString note_name(int);
 Q_UINT16 dot2len(int len, bool dot);
 void len2dot(int l, int *len, bool *dot);
+QString midi_patch_name(int);
 
+extern QString drum_abbr[128];
 extern bool isBrowserView;
