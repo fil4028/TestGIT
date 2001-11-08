b859a705c84765722102511efbd067f41211410a
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2001-11-08 12:32:43 +0000

    Re-positioned drum tuning widgets and added fictive drum names

--- kguitar/settabdrum.cpp
+++ kguitar/settabdrum.cpp
@@ -1,8 +1,6 @@
 #include "settabdrum.h"
 
 #include <klocale.h>
-#include <qcombobox.h>
-#include <qspinbox.h>
 #include <qlabel.h>
 
 SetTabDrum::SetTabDrum(QWidget *parent=0, const char *name=0)
@@ -12,17 +10,19 @@ SetTabDrum::SetTabDrum(QWidget *parent=0, const char *name=0)
 
     dr = new QSpinBox(1, MAX_STRINGS, 1, this);
     connect(dr, SIGNAL(valueChanged(int)), SLOT(stringChanged(int)));
-    dr->setGeometry(90, 50, 40, 20);
+    dr->setGeometry(90, 20, 40, 20);
 
     QLabel *dr_l = new QLabel(i18n("Drums:"), this);
-    dr_l->setGeometry(10, 50, 50, 20);
+    dr_l->setGeometry(10, 20, 50, 20);
 
     // Tuners
 
-    for (int i = 0; i < MAX_STRINGS; i++) {
+	for (int i = 0; i < MAX_STRINGS; i++) {
 		tuner[i] = new QSpinBox(this);
-		//		connect(tuner[i], SIGNAL(valueChanged(int)), SLOT(tuneChanged()));
-    }
+		tname[i] = new QLineEdit(this);
+		tname[i]->setEnabled(FALSE);
+	}
+
     oldst = MAX_STRINGS;
 }
 
@@ -32,15 +32,19 @@ void SetTabDrum::stringChanged(int n)
 		return;
 	
     if (oldst<n) {       // Need to add
-		for (int i = oldst; i < n; i++)
+		for (int i = oldst; i < n; i++) {
 			tuner[i]->show();
+			tname[i]->show();
+		}
     } else {             // Need to delete
-		for (int i = n; i < oldst; i++)
+		for (int i = n; i < oldst; i++) {
 			tuner[i]->hide();
+			tname[i]->hide();
+		}
     }
     oldst = n;
 
-    setMinimumSize(330, 90+30*n);
+    setMinimumSize(200, 90 + 25*n);
     reposTuners();
 }
 
@@ -51,6 +55,8 @@ void SetTabDrum::resizeEvent(QResizeEvent *e)
 
 void SetTabDrum::reposTuners()
 {
-    for (int i = 0; i < dr->value(); i++)
-		tuner[i]->setGeometry(10 + i * 50, 80, 50, 30);
+    for (int i = 0; i < dr->value(); i++) {
+		tuner[i]->setGeometry(10, 40 + i * 25, 50, 25);
+		tname[i]->setGeometry(70, 40 + i * 25, width() - 80, 25);
+	}
 }
