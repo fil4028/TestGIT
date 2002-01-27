334a704288baa215611f40f520cca3844e098338
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2002-01-27 02:26:31 +0000

    Got rid of evil global note duration functions

--- kguitar/global.h
+++ kguitar/global.h
@@ -8,12 +8,11 @@
 
 #include <qglobal.h>
 #include <config.h>
+#include <kdebug.h>
 
 class QString;
 
 QString note_name(int);
-Q_UINT16 dot2len(int len, bool dot);
-void len2dot(int l, int *len, bool *dot);
 QString midi_patch_name(int);
 
 extern QString drum_abbr[128];
