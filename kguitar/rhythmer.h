970a1d166f50abd4c16e9562fdfc6b60405b77f0
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2002-08-27 13:25:08 +0000

    Added rhythmer (rhythm tapping & applying constructor)

---
+++ kguitar/rhythmer.h
@@ -0,0 +1,50 @@
+#ifndef RHYTHMER_H
+#define RHYTHMER_H
+
+#include "global.h"
+#include "globaloptions.h"
+
+#include <qdialog.h>
+#include <qdatetime.h>
+
+#ifdef WITH_TSE3
+#include <tse3/MidiScheduler.h>
+#endif
+
+class QListBox;
+class QPushButton;
+class QSpinBox;
+class QCheckBox;
+
+class Rhythmer: public QDialog {
+    Q_OBJECT
+public:
+	Rhythmer(
+#ifdef WITH_TSE3
+			 TSE3::MidiScheduler *_scheduler,
+#endif
+			 QWidget *parent = 0, const char *name = 0);
+
+	QListBox *quantized;
+
+public slots:
+    void tap();
+	void reset();
+	void quantize();
+	void tempoState(bool state);
+
+private:
+	QListBox *original;
+	QSpinBox *tempo;
+	QCheckBox *temponew, *dotted, *triplet;
+
+	QPushButton *resetButton, *tapButton, *quantizeButton;
+
+	QTime time;
+
+#ifdef WITH_TSE3
+	TSE3::MidiScheduler *scheduler;
+#endif
+};
+
+#endif
