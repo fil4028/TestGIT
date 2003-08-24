92b123a060ef98e2b5050c44b8eabb1bf033e894
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2003-08-24 05:43:04 +0000

    added functional melody editor on fretboard

--- kguitar/kguitar/fretboard.cpp
+++ kguitar/kguitar/fretboard.cpp
@@ -1,16 +1,32 @@
 #include "config.h"
 #include "fretboard.h"
+#include "tabtrack.h"
 
 #include <qpainter.h>
+#include <qsizepolicy.h>
 
-#define STRING_HEIGHT    16
+#define STRING_HEIGHT    24
 #define FRET_DIVISOR     1.05946
-#define ZERO_FRET_WIDTH  32
+#define ZERO_FRET_WIDTH  24
+#define INLAY_RADIUS     5
+
+// Inlay marks array
+
+// ============  0  1  2  3  4  5  6  7  8  9 10 11 12
+bool marks[] = { 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 2,
+// ============ 13 14 15 16 17 18 19 20 21 22 23 24
+                 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 2 };
 
 Fretboard::Fretboard(TabTrack *_trk, QWidget *parent, const char *name)
+	: QWidget(parent, name)
 {
 	resize(600, 128);
 	setTrack(_trk);
+}
+
+QSizePolicy Fretboard::sizePolicy()
+{
+	return QSizePolicy(QSizePolicy::Ignored, QSizePolicy::Fixed);
 }
 
 void Fretboard::setTrack(TabTrack *_trk)
@@ -24,13 +40,49 @@ void Fretboard::paintEvent(QPaintEvent *e)
 {
 	QPainter p;
 	p.begin(this);
-	for (int i = 0; i <= trk->frets; i++) 
-		p.drawLine(fr[i], 0, fr[i], height());
+
+	p.setBrush(qRgb(205, 214, 221));
+
+	// Draw frets
+	for (int i = 0; i <= trk->frets; i++) {
+		p.drawLine((int) fr[i], 0, (int) fr[i], height());
+		// Draw inlay marks, if applicable
+		if (marks[i] == 1) {
+			p.drawEllipse((int) ((fr[i - 1] + fr[i]) / 2) - INLAY_RADIUS,
+						  height() / 2 - INLAY_RADIUS,
+						  INLAY_RADIUS * 2, INLAY_RADIUS * 2);
+		} else if (marks[i] == 2) {
+			p.drawEllipse((int) ((fr[i - 1] + fr[i]) / 2) - INLAY_RADIUS,
+						  height() / 3 - INLAY_RADIUS,
+						  INLAY_RADIUS * 2, INLAY_RADIUS * 2);
+			p.drawEllipse((int) ((fr[i - 1] + fr[i]) / 2) - INLAY_RADIUS,
+						  height() * 2 / 3 - INLAY_RADIUS,
+						  INLAY_RADIUS * 2, INLAY_RADIUS * 2);
+		}
+	}
+
+	// Draw strings
+	for (int i = 0; i < trk->string; i++)
+		p.drawLine(0, i * STRING_HEIGHT + STRING_HEIGHT / 2,
+		           width(), i * STRING_HEIGHT + STRING_HEIGHT / 2);
+
 	p.end();
 }
 
 void Fretboard::mousePressEvent(QMouseEvent *e)
 {
+	int y = trk->string - (e->y() / STRING_HEIGHT) - 1;
+	int x = 0;
+	if (e->x() > fr[0]) {
+		for (int i = 1; i <= trk->frets; i++) {
+			if (e->x() <= fr[i]) {
+				x = i;
+				break;
+			}
+		}
+	}
+	
+	emit buttonClicked(y, x, e->button());
 }
 
 void Fretboard::resizeEvent(QResizeEvent *e)
@@ -38,13 +90,20 @@ void Fretboard::resizeEvent(QResizeEvent *e)
 	recalculateSizes();
 }
 
+// Funky fret physical sizes calculation
 void Fretboard::recalculateSizes()
 {
 	double l = width() - ZERO_FRET_WIDTH;
 
-	for (int i = 0; i < trk->frets; i++) {
-		fr[i] = (int) (width() - l);
+	// Step 1: get fret sizes according to iterative algorithm
+	for (int i = 0; i <= trk->frets; i++) {
+		fr[i] = width() - l;
 		l /= FRET_DIVISOR;
 	}
-	fr[trk->frets] = width();
+
+	// Step 2: normalize total frets width to full width of widget to
+	// reclaim free space (on real guitar it's strumming area there)
+	l = ((double) width()) / ((double) (width() - l));
+	for (int i = 0; i <= trk->frets; i++)
+		fr[i] *= l;
 }
--- kguitar/kguitar/fretboard.h
+++ kguitar/kguitar/fretboard.h
@@ -2,25 +2,33 @@
 #define FRETBOARD_H
 
 #include <qwidget.h>
-#include "tabtrack.h"
 #include "global.h"
+
+class TabTrack;
+class QSizePolicy;
 
 class Fretboard: public QWidget {
     Q_OBJECT
 public:
     Fretboard(TabTrack *, QWidget *parent = 0, const char *name = 0);
+
+public slots:
 	void setTrack(TabTrack *);
+
+signals:
+	void buttonClicked(int, int, ButtonState);
 
 protected:
 	virtual void paintEvent(QPaintEvent *);
 	virtual void mousePressEvent(QMouseEvent *);
 	virtual void resizeEvent(QResizeEvent *);
+	virtual QSizePolicy sizePolicy();
 
 private:
 	void recalculateSizes();
 
 	TabTrack *trk;
-	int fr[MAX_FRETS + 1];
+	double fr[MAX_FRETS + 1]; // Proper physical fret positions
 };
 
 #endif
