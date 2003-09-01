912ea7f08372f5c9fb95a2e571d3e2b9a0a96585
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2003-09-01 08:34:56 +0000

    Made methods proper Qt slots

--- kguitar/optionspage.h
+++ kguitar/optionspage.h
@@ -5,11 +5,13 @@
 #include <qwidget.h>
 
 class OptionsPage: public QWidget {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    OptionsPage(QWidget *parent = 0, const char *name = 0);
-    virtual void applyBtnClicked() = 0;
-    virtual void defaultBtnClicked() = 0;
+	OptionsPage(QWidget *parent = 0, const char *name = 0);
+	
+public slots:
+	virtual void applyBtnClicked() = 0;
+	virtual void defaultBtnClicked() = 0;
 };
 
 #endif
