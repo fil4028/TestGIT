fe5c3447e83a6bde21f409af9ad7d4ba1b6f107f
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2003-09-04 20:05:34 +0000

    Many fixes to chord constructor, added basic chord name analyzer

---
+++ kguitar/kguitar/chordanalyzer.h
@@ -0,0 +1,25 @@
+#ifndef CHORDANALYZER_H
+#define CHORDANALYZER_H
+
+#include <global.h>
+
+class ChordAnalyzer {
+public:
+	ChordAnalyzer(QString name);
+	bool analyze();
+
+	int tonic;
+	int step[6];
+	bool result;
+	QString msg;
+
+private:
+	QString name;
+	bool fixed[6];
+	int pos;
+
+	bool checkNext(QString sample);
+	bool setStep(int step, int value, QString reason);
+};
+
+#endif
