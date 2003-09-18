325be59e4509f9c6397479be3ced3722e755f756
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2003-09-18 14:07:29 +0000

    Fixed font & spacing for first fret number

--- kguitar/kguitar/fingerlist.h
+++ kguitar/kguitar/fingerlist.h
@@ -15,29 +15,34 @@ typedef struct {
 class FingerList: public QGridView {
 	Q_OBJECT
 public:
-	FingerList(TabTrack *p, QWidget *parent=0, const char *name=0);
+	FingerList(TabTrack *p, QWidget *parent = 0, const char *name = 0);
+	~FingerList();
 
 	void addFingering(const int a[MAX_STRINGS]);
 	void clear();
 	void beginSession();
 	void endSession();
+    int count() { return appl.count(); };
+    void selectFirst();
 
 signals:
 	void chordSelected(const int *);
 
 protected:
 	virtual void paintCell(QPainter *, int row, int col);
-	virtual void resizeEvent(QResizeEvent *); 
+	virtual void resizeEvent(QResizeEvent *);
 	virtual void mousePressEvent(QMouseEvent *);
 
 private:
-	enum { SCALE=6, CIRCLE=4, CIRCBORD=1, BORDER=1, SPACER=1, FRETTEXT=5 };
+	enum { SCALE=6, CIRCLE=5, CIRCBORD=1, BORDER=1, SPACER=1, FRETTEXT=9 };
 
-	int num,perRow;
+	int num, perRow;
 	QMemArray<fingering> appl;
 
 	int curSel, oldCol, oldRow;
 	TabTrack *parm;
+
+	QFont *fretNumberFont;
 };
 
 #endif
