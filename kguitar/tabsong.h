04b8add9e13702d317ad4ab4ccbded0deafb2874
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-03-13 18:56:32 +0000

    Modularized most of load/save code to Convert* classes

--- kguitar/tabsong.h
+++ kguitar/tabsong.h
@@ -29,34 +29,31 @@ public:
 	QString transcriber;				// Who made the tab
 	QString comments;					// Comments
 
+	/**
+	 * Find the minimal free channel, for example, to use for new
+	 * track
+	 */
 	int freeChannel();
+
+	/**
+	 * Returns length of longest track in bars
+	 */
 	uint maxLen();
+
 	void arrangeBars();
 #ifdef WITH_TSE3
 	TSE3::Song *midiSong(bool tracking = FALSE);
 #endif
 
-	bool loadFromKg(QString fileName);		// Native format - kg
-	bool saveToKg(QString fileName);
 	bool loadFromGtp(QString fileName);		// Guitar Pro format
 	bool saveToGtp(QString fileName);
 	bool loadFromGp3(QString fileName);		// Guitar Pro 3 format
 	bool saveToGp3(QString fileName);
-	bool loadFromMid(QString fileName);		// MIDI files
-	bool saveToMid(QString fileName);
-	bool saveToTse3(QString fileName);        // TSE3MDL files
-	bool saveToTexTab(QString fileName);		// MusiXTeX/kgtabs.tex tabulatures
-	bool saveToTexNotes(QString fileName);	// MusiXTeX notes
-	bool loadFromXml(QString fileName);		// MusicXML format
-	bool saveToXml(QString fileName);
 
 private:
 	Q_UINT32 readVarLen(QDataStream *s);
 	void writeVarLen(QDataStream *s, uint value);
 	void writeTempo(QDataStream *s, uint value);
-	QString tab(bool chord, int string, int fret);
-	QString getNote(QString note, int duration, bool dot);
-	QString cleanString(QString str);
 
 	void readDelphiString(QDataStream *s, char *c);
 	int readDelphiInteger(QDataStream *s);
