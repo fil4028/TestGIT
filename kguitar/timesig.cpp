3df73604f4148a1960a65b0577e9b3cbbd218c1c
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-03-27 21:45:09 +0000

    Updated QDialog to KBaseDialog; Made a good API to get/set key signature with this dialog;

--- kguitar/timesig.cpp
+++ kguitar/timesig.cpp
@@ -6,56 +6,57 @@
 #include <qcombobox.h>
 #include <qcheckbox.h>
 #include <qlabel.h>
-#include <qpushbutton.h>
 #include <qlayout.h>
 
-SetTimeSig::SetTimeSig(QWidget *parent, const char *name):
-    QDialog(parent, name, TRUE)
+SetTimeSig::SetTimeSig(int t1, int t2, QWidget *parent, const char *name)
+	: KDialogBase(parent, name, TRUE, i18n("Time signature"),
+	              Ok | Cancel, Ok, TRUE)
 {
-    time1 = new QSpinBox(1,32,1,this);
-    time2 = new QComboBox(TRUE,this);
-    time2->setInsertionPolicy(QComboBox::NoInsertion);
-    time2->insertItem("1");
-    time2->insertItem("2");
-    time2->insertItem("4");
-    time2->insertItem("8");
-    time2->insertItem("16");
-    time2->insertItem("32");
+	QWidget *page = new QWidget(this);
+	setMainWidget(page);
 
-    QLabel *time1_l = new QLabel(time1,i18n("&Beats per measure:"),this);
-    QLabel *time2_l = new QLabel(time2,i18n("Beat &value:"),this);
+	m_time1 = new QSpinBox(1, 32, 1, page);
+	m_time1->setValue(t1);
 
-    QPushButton *ok = new QPushButton(i18n("OK"),this);
-    connect(ok,SIGNAL(clicked()),SLOT(accept()));
-    QPushButton *cancel = new QPushButton(i18n("Cancel"),this);
-    connect(cancel,SIGNAL(clicked()),SLOT(reject()));
+	m_time2 = new QComboBox(TRUE, page);
+	m_time2->setInsertionPolicy(QComboBox::NoInsertion);
+	m_time2->insertItem("1");
+	m_time2->insertItem("2");
+	m_time2->insertItem("4");
+	m_time2->insertItem("8");
+	m_time2->insertItem("16");
+	m_time2->insertItem("32");
 
-    QVBoxLayout *l = new QVBoxLayout(this,10);
+	switch (t2) {
+	case 1:	 m_time2->setCurrentItem(0); break;
+	case 2:	 m_time2->setCurrentItem(1); break;
+	case 4:	 m_time2->setCurrentItem(2); break;
+	case 8:	 m_time2->setCurrentItem(3); break;
+	case 16: m_time2->setCurrentItem(4); break;
+	case 32: m_time2->setCurrentItem(5); break;
+	}
 
-    QGridLayout *g = new QGridLayout(2,2,5);
-    l->addLayout(g,1);
-    g->addWidget(time1_l,0,0);
-    g->addWidget(time1,0,1);
-    g->addWidget(time2_l,1,0);
-    g->addWidget(time2,1,1);
-    g->setColStretch(0,2);
-    g->setColStretch(1,1);
-    g->addColSpacing(0,150);
-    g->addColSpacing(1,50);
-    g->addRowSpacing(0,25); g->addRowSpacing(1,25);
+	QLabel *l_time1 = new QLabel(m_time1, i18n("&Beats per measure:"), page);
+	QLabel *l_time2 = new QLabel(m_time2, i18n("Beat &value:"), page);
 
-    toend = new QCheckBox(i18n("Apply till the &end"),this);
-    toend->setMinimumSize(100,25);
-    l->addWidget(toend,1);
+	toend = new QCheckBox(i18n("Apply till the &end"),this);
+	
+	QGridLayout *l = new QGridLayout(page, 3, 2, 0, spacingHint());
+	l->addWidget(l_time1, 0, 0);
+	l->addWidget(m_time1, 0, 1);
+	l->addWidget(l_time2, 1, 0);
+	l->addWidget(m_time2, 1, 1);
+	l->addMultiCellWidget(toend, 2, 2, 0, 1);
+	
+	l->activate();
+}
 
-    QHBoxLayout *b = new QHBoxLayout(10);
-    l->addLayout(b);
-    b->addWidget(ok);
-    b->addWidget(cancel);
-    b->addStrut(30);
+int SetTimeSig::time1()
+{
+	return m_time1->value();
+}
 
-    l->activate();
-
-    resize(0,0);
-    setCaption(i18n("Time signature"));
+int SetTimeSig::time2()
+{
+	return ((QString) m_time2->currentText()).toUInt();	
 }
