4fedadb97e31e3eca5c9c2adf824555b6ed85b33
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-03-27 21:25:30 +0000

    Fixed formatting

--- kguitar_shell/kguitar.h
+++ kguitar_shell/kguitar.h
@@ -16,45 +16,45 @@ class KCommandHistory;
  * @short KGuitar Application Shell
  */
 class KGuitar: public KParts::MainWindow {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    KGuitar();
-    virtual ~KGuitar();
+	KGuitar();
+	virtual ~KGuitar();
 
 public slots:
-    void saveURL(const KURL& url);
+	void saveURL(const KURL& url);
 	void load(const KURL& url);
 
 protected slots:
 	void fileNew();
-    void fileOpen();
-    void slotToggleMainTB();
-    void slotToggleEditTB();
+	void fileOpen();
+	void slotToggleMainTB();
+	void slotToggleEditTB();
 
-    void optionsShowStatusbar();
-    void optionsConfigureKeys();
-    void optionsConfigureToolbars();
+	void optionsConfigureKeys();
+	void optionsConfigureToolbars();
+	void applyNewToolbarConfig();
 
 protected:
-    /**
-     * This method is called when it is time for the app to save its
-     * properties for session management purposes.
-     */
-    void saveProperties(KConfig *);
+	/**
+	 * This method is called when it is time for the app to save its
+	 * properties for session management purposes.
+	 */
+	void saveProperties(KConfig *);
 
-    /**
-     * This method is called when this app is restored.  The KConfig
-     * object points to the session management config file that was saved
-     * with @ref saveProperties
-     */
-    void readProperties(KConfig *);
+	/**
+	 * This method is called when this app is restored.  The KConfig
+	 * object points to the session management config file that was saved
+	 * with @ref saveProperties
+	 */
+	void readProperties(KConfig *);
 
 private:
 	void setupActions();
 
 	KParts::ReadWritePart *kgpart;
-    KRecentFilesAction *openRecentAct;
-    KToggleAction *showMainTBAct, *showEditTBAct, *showStatusbarAct;
+	KRecentFilesAction *openRecentAct;
+	KToggleAction *showMainTBAct, *showEditTBAct, *showStatusbarAct;
 	KCommandHistory *cmdHistory;
 };
 
