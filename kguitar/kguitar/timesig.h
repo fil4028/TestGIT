1b82e98ef94654fd8915e85a7790e9bb6537780b
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-03-27 21:45:09 +0000

    Updated QDialog to KBaseDialog; Made a good API to get/set key signature with this dialog;

--- kguitar/kguitar/timesig.h
+++ kguitar/kguitar/timesig.h
@@ -1,21 +1,27 @@
 #ifndef TIMESIG_H
 #define TIMESIG_H
 
-#include <qdialog.h>
+#include <kdialogbase.h>
 #include "global.h"
 
 class QSpinBox;
 class QComboBox;
 class QCheckBox;
 
-class SetTimeSig: public QDialog
-{
-    Q_OBJECT
+class SetTimeSig: public KDialogBase {
+	Q_OBJECT
+
 public:
-    SetTimeSig(QWidget *parent=0, const char *name=0);
-    QSpinBox *time1;
-    QComboBox *time2;
-    QCheckBox *toend;
+	SetTimeSig(int t1, int t2, QWidget *parent = 0, const char *name = 0);
+
+	int time1();
+	int time2();
+
+	QCheckBox *toend;
+
+protected:
+	QSpinBox *m_time1;
+	QComboBox *m_time2;
 };
 
 #endif
