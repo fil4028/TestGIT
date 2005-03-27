e6b497cb6b9851f85d95392ccc34206ff478f2e5
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-03-27 08:36:24 +0000

    Fixed formatting

--- kguitar/kguitar_shell/main.cpp
+++ kguitar/kguitar_shell/main.cpp
@@ -22,9 +22,9 @@ static const char *DESCRIPTION = I18N_NOOP("A stringed instrument tabulature edi
 int main(int argc, char **argv)
 {
 	KAboutData about("kguitar", "KGuitar",
-						 VERSION, DESCRIPTION, KAboutData::License_GPL,
-						 "(C) 2000-2003 by KGuitar Development Team", 0,
-						 "http://kguitar.sourceforge.net");
+	                 VERSION, DESCRIPTION, KAboutData::License_GPL,
+	                 "(C) 2000-2003 by KGuitar Development Team", 0,
+	                 "http://kguitar.sourceforge.net");
 
 	about.addAuthor("Mikhail Yakshin AKA GreyCat", I18N_NOOP("Maintainer and main coder"),
 						"greycat@users.sourceforge.net");
@@ -37,18 +37,18 @@ int main(int argc, char **argv)
 	about.addCredit("Wilane Ousmane", 0, "wilane@users.sourceforge.net");
 	about.addCredit("Richard G. Roberto", 0, "robertor@users.sourceforge.net");
 	about.addCredit("Riccardo Vitelli AKA feac", 0, "feac@users.sourceforge.net");
-    about.addCredit(0, I18N_NOOP("Special Thanks to Ronald Gelten who\n"
-                        "allowed us to make changes to tabdefs.tex"), 0);
+	about.addCredit(0, I18N_NOOP("Special Thanks to Ronald Gelten who\n"
+	                "allowed us to make changes to tabdefs.tex"), 0);
 
 	KCmdLineArgs::init(argc, argv, &about);
 	KCmdLineArgs::addCmdLineOptions(options);
 
 	KApplication app;
 
-    // see if we are starting with session management
-	if (app.isRestored())
+	// see if we are starting with session management
+	if (app.isRestored()) {
 		RESTORE(KGuitar)
-	else {
+	} else {
         // no session.. just start up normally
 		KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
 
