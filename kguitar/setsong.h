1a22d760e32bb628eb72369d405dba1ef99542a1
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-04-02 00:00:11 +0000

    Reworked to be KDE-based dialog and provide good interface in new QMap-based API terms

--- kguitar/setsong.h
+++ kguitar/setsong.h
@@ -1,24 +1,36 @@
 #ifndef SETSONG_H
 #define SETSONG_H
 
-#include <qdialog.h>
+#include <kdialogbase.h>
 #include "global.h"
+
+#include <qmap.h>
+#include <qstring.h>
+#include <knuminput.h>
 
 class QLineEdit;
 class QMultiLineEdit;
 class KIntNumInput;
 
-class SetSong: public QDialog
-{
+/**
+ * Song properties dialog.
+ *
+ * Provides song properties dialog that maintains all metainformation
+ * about song and, for now, holds first tempo value.
+ */
+class SetSong: public KDialogBase {
     Q_OBJECT
 public:
-    SetSong(QWidget *parent=0, const char *name=0);
+    SetSong(QMap<QString, QString> info, int tempo_, bool ro, QWidget *parent=0, const char *name=0);
+	QMap<QString, QString> info();
+	int tempo() { return m_tempo->value(); }
 
+private:
     QLineEdit *title, *author, *transcriber;
     QMultiLineEdit *comments;
+	KIntNumInput *m_tempo;
 
-	KIntNumInput *tempo;
-private:
+	QMap<QString, QString> m_info;
 };
 
 #endif
