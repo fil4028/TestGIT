f40c327dfd00c0331a7997de110b06d11c847855
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-04-02 00:00:11 +0000

    Reworked to be KDE-based dialog and provide good interface in new QMap-based API terms

--- kguitar/kguitar/setsong.cpp
+++ kguitar/kguitar/setsong.cpp
@@ -1,34 +1,32 @@
 #include "setsong.h"
 
 #include <klocale.h>
-#include <knuminput.h>
 
 #include <qlineedit.h>
 #include <qmultilinedit.h>
 #include <qlabel.h>
-#include <qpushbutton.h>
 #include <qlayout.h>
 
-SetSong::SetSong(QWidget *parent, const char *name): QDialog(parent, name, TRUE)
+SetSong::SetSong(QMap<QString, QString> info, int tempo_, bool ro, QWidget *parent, const char *name)
+	: KDialogBase(parent, name, TRUE, i18n("Song properties"),
+	              Ok | Cancel, Ok, TRUE)
 {
-	title = new QLineEdit(this);
-	author = new QLineEdit(this);
-	transcriber = new QLineEdit(this);
-	comments = new QMultiLineEdit(this);
-	tempo = new KIntNumInput(this);
+	QWidget *page1 = new QWidget(this);
+	setMainWidget(page1);
+	
+	title = new QLineEdit(page1);
+	author = new QLineEdit(page1);
+	transcriber = new QLineEdit(page1);
+	comments = new QMultiLineEdit(page1);
+	m_tempo = new KIntNumInput(page1);
 
-	QLabel *title_l = new QLabel(title, i18n("&Title:"), this);
-	QLabel *author_l = new QLabel(author, i18n("&Author:"), this);
-	QLabel *transcriber_l = new QLabel(transcriber, i18n("&Transcriber:"), this);
-	QLabel *comments_l = new QLabel(comments, i18n("&Comments:"), this);
-	QLabel *tempo_l = new QLabel(tempo, i18n("T&empo:"), this);
+	QLabel *title_l = new QLabel(title, i18n("&Title:"), page1);
+	QLabel *author_l = new QLabel(author, i18n("&Artist:"), page1);
+	QLabel *transcriber_l = new QLabel(transcriber, i18n("&Transcriber:"), page1);
+	QLabel *comments_l = new QLabel(comments, i18n("&Comments:"), page1);
+	QLabel *tempo_l = new QLabel(m_tempo, i18n("T&empo:"), page1);
 
-	QPushButton *ok = new QPushButton(i18n("OK"), this);
-	connect(ok, SIGNAL(clicked()), SLOT(accept()));
-	QPushButton *cancel = new QPushButton(i18n("Cancel"), this);
-	connect(cancel, SIGNAL(clicked()), SLOT(reject()));
-
-	QGridLayout *g = new QGridLayout(this, 6, 2, 10);
+	QGridLayout *g = new QGridLayout(page1, 6, 2, 0, spacingHint());
 	g->addWidget(title_l, 0, 0);
 	g->addWidget(title, 0, 1);
 	g->addWidget(author_l, 1, 0);
@@ -38,23 +36,29 @@ SetSong::SetSong(QWidget *parent, const char *name): QDialog(parent, name, TRUE)
 	g->addWidget(comments_l, 3, 0);
 	g->addWidget(comments, 3, 1);
 	g->addWidget(tempo_l, 4, 0);
-	g->addWidget(tempo, 4, 1);
-
-	QHBoxLayout *butt = new QHBoxLayout();
-	g->addLayout(butt, 5, 1);
-	butt->addWidget(ok);
-	butt->addWidget(cancel);
-
-	g->setRowStretch(3, 1);
-	g->setColStretch(1, 1);
-	g->addColSpacing(0, 90);
-	for (int i = 0; i < 4; i++)
-		g->addRowSpacing(i, 20);
-	g->addRowSpacing(4, 30);
+	g->addWidget(m_tempo, 4, 1);
 
 	g->activate();
 
-	setMinimumSize(250, 170);
-	resize(400, 300);
-	setCaption(i18n("Song properties"));
+	title->setText(info["TITLE"]);
+ 	title->setReadOnly(ro);
+	author->setText(info["ARTIST"]);
+ 	author->setReadOnly(ro);
+	transcriber->setText(info["TRANSCRIBER"]);
+ 	transcriber->setReadOnly(ro);
+	comments->setText(info["COMMENTS"]);
+ 	comments->setReadOnly(ro);
+	m_tempo->setValue(tempo_);
+	//	tempo->setReadOnly(ro); // GREYFIX - what the heck about KIntNumInput???
+
+	m_info = info;
+}
+
+QMap<QString, QString> SetSong::info()
+{
+	m_info["TITLE"] = title->text();
+	m_info["ARTIST"] = author->text();
+	m_info["TRANSCRIBER"] = transcriber->text();
+	m_info["COMMENTS"] = comments->text();
+	return m_info;
 }
