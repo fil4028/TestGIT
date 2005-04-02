aaab86122ffc305cf77313b156c6c0d61ea5cecd
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2005-04-02 00:00:39 +0000

    Added docs

--- kguitar/kguitar/chordlist.h
+++ kguitar/kguitar/chordlist.h
@@ -6,13 +6,16 @@
 
 #include "chordlistitem.h"
 
-class ChordList: public QListBox
-{
-    Q_OBJECT
+/**
+ * Special QListBox that holds ChordListItem objects and keeps them
+ * sorted.
+ */
+class ChordList: public QListBox {
+	Q_OBJECT
 public:
-    ChordList(QWidget *parent=0, const char *name=0);
-    ChordListItem* currentItemPointer();
-    void inSort(ChordListItem *it);
+	ChordList(QWidget *parent=0, const char *name=0);
+	ChordListItem* currentItemPointer();
+	void inSort(ChordListItem *it);
 };
 
 #endif
