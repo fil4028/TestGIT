2fc2343e3da7429ed68324d9f1897846ebe7f176
greycat <greycat@1d129ebe-d642-0410-a05f-df7310e4b27a>
2009-03-11 09:18:03 +0000

    Proper Q3 types

--- kguitar/optionsmelodyeditor.h
+++ kguitar/optionsmelodyeditor.h
@@ -5,7 +5,7 @@
 #include "global.h"
 #include <ksharedconfig.h>
 
-class Q3ButtonGroup;
+class Q3VButtonGroup;
 class QRadioButton;
 class QComboBox;
 class QCheckBox;
@@ -20,7 +20,7 @@ public slots:
 	virtual void defaultBtnClicked();
 
 private:
-	Q3ButtonGroup *inlayGroup, *woodGroup;
+	Q3VButtonGroup *inlayGroup, *woodGroup;
 	QRadioButton *inlay[6], *wood[4];
 	QComboBox *mouseAction[3];
 	QCheckBox *mouseAdvance[3];
